b611388e48f616f4220a8fd7401380065e0294d3
Amanda5335 <nanasfp1@gmail.com>
2023-01-19 19:54:21 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const jobSelect = document.querySelector("#job")
+const messageTextarea = document.querySelector("#message")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (nameInput.value === "") {
+        alert("preencha o seu nome");
+        return;
+    }
+
+    if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("preencha o seu email");
+        return;
+    }
+
+    // Verificar se está preenchido
+    if (!validatePassword(passwordInput.value, 8)) {
+        alert("A senha precisa ser no mínimo 8 dígitos.");
+        return;
+    }
+
+    if (jobSelect.value === "") {
+        alert("Selecione a sua situação");
+        return;
+    }
+
+    if (messageTextarea.valeu === "") {
+        alert("Por favor, escreva uma mensagem");
+        return;
+    }
+
+    // se estiver tudo preenchido envie o form
+    form.submit();
+});
+
+// função que valida email 
+function isEmailValid(email) {
+
+    //usando uma regex pra validar
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}/
+    );
+
+    if (emailRegex.test(email)) {
+        return true;
+    }
+
+    return false;
+}
+
+// função que valida a senha 
+function validatePassword(password, minDigits) {
+    if (password.length >= minDigits) {
+        return true;
+    }
+
+    return false;
+}
